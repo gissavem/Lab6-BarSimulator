25c6abbd03d901a7846424b613b447b9aa43eb68
John Andersson <john.andersson1995@gmail.com>
2019-10-23 10:10:33 +0200

    change order of names (again)

--- Lab6/Lab6/NameList.cs
+++ Lab6/Lab6/NameList.cs
@@ -11,8 +11,8 @@ namespace Lab6
     {
         public static BlockingCollection<string> AvailableNames = new BlockingCollection<string>
         { 
-            "Astrid Lindgren", "Lille Skutt", "Jesus Kristus", "Fjodor Dostojevski", "Greta Garbo", "Amy Diamond",
             "John Andersson", "Alexander Bertillson", "Emil Martini", "Daniel Andersson", "Pontus Lindgren",
+            "Astrid Lindgren", "Lille Skutt", "Jesus Kristus", "Fjodor Dostojevski", "Greta Garbo", "Amy Diamond",
             "Olof Palme", "Berit Bredstjärt", "Dustin Hoffman", "Sylvester Stalone", "Angela Merkel", "Fredrik Reinfeldt",
             "Ett Fyllesvin", "Lasse Åberg", "Ben Rangel", "Didrik Fetknopp", "Wildkids Ola", "Gusten Grodslukare", 
             "Torsten Flinck", "Sebbe Staxx", "Lisa Lusis", "Lennart Bladh", "Kajsa Warg", "Tupac Shakur", "Augustus Ceasar"
