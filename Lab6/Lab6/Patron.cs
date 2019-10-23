62de4297275a9dd40c117f0b8f5c8b6b84f82978
John Andersson <john.andersson1995@gmail.com>
2019-10-23 13:55:15 +0200

    start implementing simulation of patrons

--- Lab6/Lab6/Patron.cs
+++ Lab6/Lab6/Patron.cs
@@ -12,7 +12,20 @@ namespace Lab6
         }
         public override void Simulate()
         {
-            throw new NotImplementedException();
+            if (Beer == null)
+            {
+                //wait for beer
+                return;
+            }
+            if (Beer.HasBeer == true)
+            {
+
+            }
+            else if (Beer.IsDirty == true)
+            {
+
+            }
+            
         }
 
         public override void GoHome()
